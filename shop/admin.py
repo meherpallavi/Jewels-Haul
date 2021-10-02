306b86398bc9176f927c9f20fefe0480ae3ade64
pallavi <meherpallavi.26@gmail.com>
2021-10-02 15:56:10 +0530

    firstchange

---
+++ shop/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Product
+from .models import Order
+
+
+admin.site.register(Product)
+admin.site.register(Order)
+
+
+# Register your models here.
